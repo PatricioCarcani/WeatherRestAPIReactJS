13e020f31e6316a3ba0df8449d42374f95b2298f
Patricio Carcani <carcani.patricio@gmail.com>
2019-03-17 23:17:48 -0300

    Primer commit

---
+++ precache-manifest.e1c9ce2f3d94e43f5e435d8b60c68aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "227828fa3bafc134e335",
+    "url": "/static/js/main.87d7288f.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "227828fa3bafc134e335",
+    "url": "/static/css/main.cd7d837e.chunk.css"
+  },
+  {
+    "revision": "f873a2d900c7406da913ac182ea23790",
+    "url": "/index.html"
+  }
+];
